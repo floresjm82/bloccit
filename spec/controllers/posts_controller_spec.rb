8c4bae5a6b58da71ec1c4908ae3a41b06cd234aa
Jorge <floresjmjr@gmail.com>
2015-09-20 17:33:42 -0400

    posts can now be created, updated and shown

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -25,22 +25,73 @@ are available for the view.
     end
 
   end
-=begin
-10 - we comment out the tests for show, new, and edit since we wont write the
-implementation until later.
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
 
-  describe "GET #new" do
+  describe "GET new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
+# we expect PostsController#new to render the posts new view. We use the
+# render_template method to verify that the correct template (view) is rendered.
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+# we expect the @post instance variable to be initialized by PostsController#new.
+# assigns gives us access to the @post variable, assigning it to :post.
+    it "instantiates @post" do
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
   end
+
+  describe "POST create" do
+# we expect that after PostsController#create is called with the parameters
+# {post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}},
+# the count of Post instances (i.e. rows in the posts table) in the database
+# will increase by one.
+    it "increases the number of Post by 1" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    end
+# when create is POSTed to, we expect the newly created post to be assigned to @post.
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(assigns(:post)).to eq Post.last
+    end
+# we expect to be redirected to the newly created post.
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+#we pass {id: my_post.id} to show as a parameter. These parameters are passed to
+#the params hash. The params hash contains all parameters passed to the application
+# controller (application_controller.rb) whether from GET, POST, or any other HTTP action.
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+# we expect the response to return the show view using the render_template matcher.
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+# we expect the post to equal my_post because we call show with the id of my_post.
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+
+
+=begin
+10 - we comment out the tests for show, new, and edit since we wont write the
+implementation until later.
+
 
   describe "GET #edit" do
     it "returns http success" do
