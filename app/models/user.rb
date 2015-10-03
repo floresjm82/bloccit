ac6f79606d79dd9f3cfbedef91093db3c3fcfc11
Jorge <floresjmjr@gmail.com>
2015-10-03 18:21:56 -0400

    added format name callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,33 @@ class User < ActiveRecord::Base
 # Callbacks are hooks into the life cycle of an Active Record object that allow
 # you to trigger logic before or after an aleration of the object state.
 
-  before_save { self.email = email.downcase }
+  before_save do
+    self.email = email.downcase
+
+# self.name = (name.split.each { |n| n.capitalize!}).join(" ")  ????
+    n_array = name.split
+    n_capitalize = []
+    n_array.each do |n|
+      n_capitalize << n.capitalize!
+    end
+    self.name = n_capitalize.join(" ")
+  end
+
 
 # we set a variable to a regular expression, which defines a specific character
 # pattern that we want to match against a string. The character pattern that we
 # set EMAIL_REGEX to defines what constitutes a valid email address.
 
-     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-   validates :password, length: { minimum: 6 }, allow_blank: true
-   validates :email,
-      presence: true,
-      uniqueness: { case_sensitive: false },
-      length: { minimum: 3, maximum: 100 },
-      format: { with: EMAIL_REGEX }
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 100 },
+            format: { with: EMAIL_REGEX }
 
 # has_secure_password adds methods to set and authenticate against a BCrypt password.
 # creates two virtual attributes, password and password_confirmation that we use
