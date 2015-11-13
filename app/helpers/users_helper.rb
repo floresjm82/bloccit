bcbcd457a312742b1ca66185c51bfae8936d5bb0
Jorge <floresjmjr@gmail.com>
2015-11-12 19:55:24 -0500

    favorite posts, helper method but missing gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,33 @@
 module UsersHelper
+
+  def render_posts_for(user)
+    if user.posts.any?
+      render(@user.posts).html_safe
+    else
+      "<p>No posts have been submitted by #{user.name}</p>".html_safe
+    end
+  end
+
+  def render_comments_for(user)
+    if user.comments.any?
+      output = "<ul>"
+      user.comments.each do |comment|
+        output += "<li>#{comment.body} posted on #{comment.post.title}</li>"
+      end
+      output += "</ul>"
+      return output.html_safe
+    else
+      "<p>No comments have been submitted by #{user.name}</p>".html_safe
+    end
+  end
+
+  def render_favorite_posts_for(user)
+    if user.favorites.any?
+      render(@user.favorites.collect{|favorite| favorite.post}).html_safe
+    else
+      "<p>#{user.name} has not favorited any posts</p>".html_safe
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,12 @@ Rails.application.routes.draw do
 # don't need new or index routes as comments are displayed on the posts show view
      resources :comments, only: [:create, :destroy]
      resources :favorites, only: [:create, :destroy]
+     post '/up-vote' => 'votes#up_vote', as: :up_vote
+     post '/down-vote' => 'votes#down_vote', as: :down_vote
 
    end
 
-   post '/up-vote' => 'votes#up_vote', as: :up_vote
-   post '/down-vote' => 'votes#down_vote', as: :down_vote
+
 
 
 # we create routes for new and create actions...only hash key will preven Rails
